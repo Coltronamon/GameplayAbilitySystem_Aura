f12d20331a5e5bc9d1789ad493db0fb7222b7cbc
Coltronamon <coltonmgk@gmail.com>
2025-10-01 23:32:01 -0400

    Highlight Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,11 +4,77 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
 	
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 *Line trace from cursor. There are several scenarios:
+	 * A. LasActor is null and ThisActor is null
+	 *    - Do nothing.
+	 * B. LastActor is null && ThisActor is valid
+	 *    - Highlight ThisActor
+	 * C. LastActor is valid && ThisActor is null
+	 *     - UnHighlight LAstActor
+	 * D. Both actors are valid, but LAstActor != ThisActor
+	 *     - UnHighlight LastActor, and Highlight ThisActor
+	 * E. Both actors are valid, and are the same actor
+	 *     - Do nothing
+	 */
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A - both are null, do nothing - Case A
+		}
+	}
+	else// LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnhighlightActor();
+		}
+		else // both actors are valid
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnhighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case E - do nothing
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -53,3 +119,5 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPAwn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
+
+
