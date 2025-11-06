3c267fbdd1d4af752398ace5cc074977a54dd83e
Coltronamon <coltonmgk@gmail.com>
2025-11-06 12:06:33 -0500

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyrite Colton Kearschner
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyrite Colton Kearschner
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
