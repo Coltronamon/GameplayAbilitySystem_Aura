54293de8790fd69c2d65d53b8ea858a0567c0041
Coltronamon <coltonmgk@gmail.com>
2025-11-19 21:07:45 -0500

    Created attribute info delegate

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,6 +6,10 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSigniture, const FAuraAttributeInfo&, Info);
+
 /**
  * 
  */
@@ -16,4 +20,12 @@ class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 public:
 	virtual void BindCallbacksToDependencies() override;
 	virtual void BroadcastInitialValues() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FAttributeInfoSigniture AttributeInfoDelegate;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 };
