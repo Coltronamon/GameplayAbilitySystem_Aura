5c022c2abca2bae59ce99eac1544275c49e82760
Coltronamon <coltonmgk@gmail.com>
2026-01-12 21:14:54 -0500

    Created logic to spawn a projectile for the firebolt ability

--- Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -5,11 +5,4 @@
 
 #include "Kismet/KismetSystemLibrary.h"
 
-void UAuraGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-                                           const FGameplayEventData* TriggerEventData)
-{
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	UKismetSystemLibrary::PrintString(this, FString("Activate Ability (C++)"), true, true, FLinearColor::Yellow, 3);
-}
