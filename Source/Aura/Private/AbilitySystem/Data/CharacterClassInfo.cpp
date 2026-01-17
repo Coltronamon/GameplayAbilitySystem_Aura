7c251fb5d57975e2f936126d37614f7090c03b6a
Coltronamon <coltonmgk@gmail.com>
2026-01-16 19:31:22 -0500

    Added a character class info data table for setting up defualt attributes for character classes

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyrite Colton Kearschner
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
