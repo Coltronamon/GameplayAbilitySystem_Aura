dfc6c99fed41b2c7e4f24b9f10766b2ad139212f
Coltronamon <coltonmgk@gmail.com>
2026-01-19 15:08:06 -0500

    Created a combat interface function for causin death on enemies

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -25,7 +25,10 @@ public:
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
 	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
+	virtual void Die() override;
 
+	UFUNCTION(NetMulticast, Reliable)
+	virtual void MultucastHandleDeath();
 protected:
 	virtual void BeginPlay() override;
 
