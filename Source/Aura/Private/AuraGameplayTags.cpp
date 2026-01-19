f85863b5d65dd543d4424be101a5a8f629c41328
Coltronamon <coltonmgk@gmail.com>
2026-01-18 22:53:39 -0500

    created a set by caller magnitude for damage to be used in gameplay abilities

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -116,4 +116,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Input Tag for 4 key")
 		);
 
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Damage"),
+		FString("Damage")
+		);
+
 }
